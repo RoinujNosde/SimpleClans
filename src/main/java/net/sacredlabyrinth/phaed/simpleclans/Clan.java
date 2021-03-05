157116571fe543ab652a008e12f52d19d0a8a935
FireController1847 <usfirepilot123@gmail.com>
2021-03-05 00:05:47 -0700

    A sysout is good for debugging, no so much for production

--- src/main/java/net/sacredlabyrinth/phaed/simpleclans/Clan.java
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/Clan.java
@@ -243,7 +243,6 @@ public class Clan implements Serializable, Comparable<Clan> {
      */
     @Placeholder("tag_color")
     public String getTagColors() {
-        System.out.println("COLOR TAG: " + colorTag.replace("\u00a7", "&"));
         if (colorTag.startsWith("\u00a7x")) { // Hexadecimal
             return colorTag.substring(0, 14);
         } else if (colorTag.charAt(0) == '\u00a7') { // Regular Codes
