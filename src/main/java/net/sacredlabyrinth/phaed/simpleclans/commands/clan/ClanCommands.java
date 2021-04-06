37eba482edb407863ef8633728f1c45e57ecc13d
Minat0_ <minat0_@outlook.com>
2021-04-06 19:04:32 +0300

    Hide vanished players from the invite command #128

--- src/main/java/net/sacredlabyrinth/phaed/simpleclans/commands/clan/ClanCommands.java
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/commands/clan/ClanCommands.java
@@ -127,11 +127,11 @@ public class ClanCommands extends BaseCommand {
 
     @Subcommand("%invite")
     @CommandPermission("simpleclans.leader.invite")
-    @CommandCompletion("@non_members")
+    @CommandCompletion("@non_vanished_members")
     @Conditions("rank:name=INVITE")
     @Description("{@@command.description.invite}")
     public void invite(Player sender, ClanPlayer cp, Clan clan,
-                       @Conditions("not_banned|not_in_clan|online") @Name("player") ClanPlayerInput invited) {
+                       @Conditions("not_banned|not_in_clan|online|not_in_vanish") @Name("player") ClanPlayerInput invited) {
         Player invitedPlayer = invited.getClanPlayer().toPlayer();
         if (invitedPlayer == null) return;
         if (!permissions.has(invitedPlayer, "simpleclans.member.can-join")) {
