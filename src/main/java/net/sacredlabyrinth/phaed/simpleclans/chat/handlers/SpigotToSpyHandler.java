5730dab26da96d565b3d4e55f12600628aa1211d
Minat0_ <minat0_@outlook.com>
2021-08-05 18:09:32 +0300

    Moves getOnlineClanPlayers() from ClanManager to SpigotToSpyHandler

--- src/main/java/net/sacredlabyrinth/phaed/simpleclans/chat/handlers/SpigotToSpyHandler.java
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/chat/handlers/SpigotToSpyHandler.java
@@ -2,8 +2,10 @@ package net.sacredlabyrinth.phaed.simpleclans.chat.handlers;
 
 import net.sacredlabyrinth.phaed.simpleclans.ChatBlock;
 import net.sacredlabyrinth.phaed.simpleclans.ClanPlayer;
+import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
 import net.sacredlabyrinth.phaed.simpleclans.chat.ChatHandler;
 import net.sacredlabyrinth.phaed.simpleclans.chat.SCMessage;
+import org.bukkit.Bukkit;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,7 +20,7 @@ public class SpigotToSpyHandler implements ChatHandler {
         String format = plugin.getSettingsManager().getString(ConfigField.valueOf(message.getChannel() + "CHAT_SPYFORMAT"));
         String formattedMessage = plugin.getChatManager().parseChatFormat(format, message);
 
-        List<ClanPlayer> onlinePlayers = plugin.getClanManager().getOnlineClanPlayers().stream()
+        List<ClanPlayer> onlinePlayers = getOnlineClanPlayers().stream()
                 .filter(player -> plugin.getPermissionsManager().has(player.toPlayer(), "simpleclans.admin.all-seeing-eye"))
                 .filter(player -> !player.isMuted())
                 .collect(Collectors.toList());
@@ -29,5 +31,11 @@ public class SpigotToSpyHandler implements ChatHandler {
     @Override
     public boolean canHandle(SCMessage.Source source) {
         return source == SPIGOT;
+    }
+
+    private List<ClanPlayer> getOnlineClanPlayers() {
+        return SimpleClans.getInstance().getClanManager().getAllClanPlayers().stream().
+                filter(cp -> Bukkit.getOfflinePlayer(cp.getUniqueId()).isOnline()).
+                collect(Collectors.toList());
     }
 }
