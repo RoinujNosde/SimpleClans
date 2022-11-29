62f3bc41a768338f76219625cb7c7f64fd65d349
Minat0_ <minat0_@outlook.com>
2022-11-29 20:46:15 +0300

    fix: missing getHandlerList on HomeRegroupEvent

--- src/main/java/net/sacredlabyrinth/phaed/simpleclans/events/HomeRegroupEvent.java
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/events/HomeRegroupEvent.java
@@ -50,12 +50,16 @@ public class HomeRegroupEvent extends Event implements Cancellable {
     public HandlerList getHandlers() {
         return handlers;
     }
-
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    
     @Override
     public boolean isCancelled() {
         return cancelled;
     }
-
+    
     @Override
     public void setCancelled(boolean cancel) {
         cancelled = cancel;
