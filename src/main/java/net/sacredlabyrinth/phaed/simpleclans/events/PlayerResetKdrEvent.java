c0e5342f41f6b62bf5656c682190d815bf873435
ThiagoROX <thiagokenis@live.com>
2023-04-28 09:54:19 -0300

    remove ClanPlayerEvent

--- src/main/java/net/sacredlabyrinth/phaed/simpleclans/events/PlayerResetKdrEvent.java
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/events/PlayerResetKdrEvent.java
@@ -1,16 +1,15 @@
 package net.sacredlabyrinth.phaed.simpleclans.events;
 
-
-import net.sacredlabyrinth.phaed.simpleclans.ClanPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author ThiagoROX
  */
-public class PlayerResetKdrEvent extends ClanPlayerEvent implements Cancellable {
+public class PlayerResetKdrEvent extends PlayerEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
     private boolean cancelled;
@@ -18,10 +17,10 @@ public class PlayerResetKdrEvent extends ClanPlayerEvent implements Cancellable 
     /**
      * Event called when a player has their KDR reset
      *
-     * @param player The player whose kill death rate was reset
+     * @param who The player whose kill death rate was reset
      */
-    public PlayerResetKdrEvent(@NotNull Player player, ClanPlayer clanPlayer) {
-        super(player, clanPlayer);
+    public PlayerResetKdrEvent(@NotNull Player who) {
+        super(who);
     }
 
     @Override
