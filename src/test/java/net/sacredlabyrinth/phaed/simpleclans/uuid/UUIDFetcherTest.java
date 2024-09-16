e36ad06d54b787dbe0339c649f0f7a67d8d5fdb8
Peng1104 <admin@peng1104.net>
2024-09-16 18:59:24 -0300

    feat(uuid): added error handling for Mojang API throttling

--- src/test/java/net/sacredlabyrinth/phaed/simpleclans/uuid/UUIDFetcherTest.java
+++ src/test/java/net/sacredlabyrinth/phaed/simpleclans/uuid/UUIDFetcherTest.java
@@ -9,21 +9,43 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UUIDFetcherTest {
-
+    
     private UUID ghostUUID;
-
+    
     @BeforeEach
     public void setup() {
         ghostUUID = UUID.fromString("bfe9a44f-e44d-453b-8219-74eb186c3932");
     }
-
+    
     @Test
     public void getUUIDOf() throws Exception {
-        assertEquals(ghostUUID, UUIDFetcher.getUUIDOf("GhostTheWolf"));
+        try {
+            assertEquals(ghostUUID, UUIDFetcher.getUUIDOf("GhostTheWolf"));
+        }
+        catch (IOException e) {
+            if (e.getMessage().startsWith("Server returned HTTP response code: 403 for URL")) {
+                System.out.println(
+                        "You have been throttled by Mojang's API. Please wait a few minutes before trying again.");
+            }
+            else {
+                throw e;
+            }
+        }
     }
-
+    
     @Test
     public void getUUIDOfThrottled() throws IOException, InterruptedException {
-        assertEquals(ghostUUID, UUIDFetcher.getUUIDOfThrottled("GhostTheWolf"));
+        try {
+            assertEquals(ghostUUID, UUIDFetcher.getUUIDOfThrottled("GhostTheWolf"));
+        }
+        catch (IOException e) {
+            if (e.getMessage().startsWith("Server returned HTTP response code: 403 for URL")) {
+                System.out.println(
+                        "You have been throttled by Mojang's API. Please wait a few minutes before trying again.");
+            }
+            else {
+                throw e;
+            }
+        }
     }
 }
