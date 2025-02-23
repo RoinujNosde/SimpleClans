081056e4d35a5c488ff40b15e66cfa51ad4858d3
RoinujNosde <edsonpassosjr@outlook.com>
2025-02-23 17:36:09 -0300

    feat: only send bungee messages to allowed servers

---
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/proxy/listeners/Broadcast.java
@@ -0,0 +1,18 @@
+package net.sacredlabyrinth.phaed.simpleclans.proxy.listeners;
+
+import com.google.common.io.ByteArrayDataInput;
+import net.sacredlabyrinth.phaed.simpleclans.proxy.BungeeManager;
+import org.bukkit.Bukkit;
+
+public class Broadcast extends MessageListener {
+
+    public Broadcast(BungeeManager bungee) {
+        super(bungee);
+    }
+
+    @Override
+    public void accept(ByteArrayDataInput data) {
+        String message = data.readUTF();
+        Bukkit.getOnlinePlayers().forEach(p -> p.sendMessage(message));
+    }
+}
